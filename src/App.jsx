312b60348f1245bd1ca645005aae353ebd57f2b2
GhulamShubhaniGfuture <ghulam.gfuture@gmail.com>
2025-05-22 01:37:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is test {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and savenfiiiiifjgfghjdf to test HMR
+          Edit <code>src/App.jsx</code> and savenfiiii888ifjgfghjdf to test HMR
         </p>
       </div>
       <p className="read-the-docs">
